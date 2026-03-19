0891bd5593145d14dea4c5b30d49ddb436a80fa9
FallingAngel <the.fallenangel.965@gmail.com>
2026-03-19 11:02:31 +0800

    refactor(collate): 调整数据设备移动逻辑使用惯例
    为了提高代码的可维护性和可读性，调整了数据设备移动的逻辑。通过遵循常见的惯例，避免在函数返回时进行设备移动操作。这种调整方法不仅使代码更加整洁，还减少了在设备移动时可能出现的错误。

--- src/word_ferry/components/collate.py
+++ src/word_ferry/components/collate.py
@@ -1,7 +1,6 @@
 import torch
 from torch.nn import functional as torch_f
 
-from src.word_ferry.components.config import Config
 from src.word_ferry.components.dataset import TokenizedTransSample, BatchedTransSample
 from src.word_ferry.core.constants import PAD_TOKEN_ID
 
@@ -38,32 +37,32 @@ def collate_fn(batch: list[TokenizedTransSample]) -> BatchedTransSample:
     padded_tgt_in = []
     padded_tgt_out = []
     tgt_attention_mask = []
-    
+
     for tgt in tgt_list:
         # 去掉最后一个token作为解码器输入 [BOS] + tokens[:-1]
         tgt_in = tgt[:-1]
         # 去掉第一个token作为目标输出 tokens + [EOS]
         tgt_out = tgt[1:]
-    
+
         # Padding
         in_padding_len = tgt_max_len - len(tgt_in)
         out_padding_len = tgt_max_len - len(tgt_out)
-    
+
         # 文本右填充
         padded_in = torch_f.pad(tgt_in, (0, in_padding_len), value=PAD_TOKEN_ID)
         padded_out = torch_f.pad(tgt_out, (0, out_padding_len), value=PAD_TOKEN_ID)
-    
+
         # mask = 原始长度的1 + 填充长度的0
         mask = torch.cat([torch.ones(len(tgt_in)), torch.zeros(out_padding_len)])
-    
+
         padded_tgt_in.append(padded_in)
         padded_tgt_out.append(padded_out)
         tgt_attention_mask.append(mask)
 
     return BatchedTransSample(
-        torch.stack(padded_src).to(Config.device),
-        torch.stack(src_attention_mask).to(Config.device),
-        torch.stack(padded_tgt_in).to(Config.device),
-        torch.stack(padded_tgt_out).to(Config.device),
-        torch.stack(tgt_attention_mask).to(Config.device),
+        torch.stack(padded_src),
+        torch.stack(src_attention_mask),
+        torch.stack(padded_tgt_in),
+        torch.stack(padded_tgt_out),
+        torch.stack(tgt_attention_mask),
     )
