0ac2d7f67734bde3f07cd5a754fc9b2f1d9c3117
FallingAngel <the.fallenangel.965@gmail.com>
2026-03-22 11:16:52 +0800

    feat(logger): 优化日志目录管理

--- src/word_ferry/components/logger.py
+++ src/word_ferry/components/logger.py
@@ -4,11 +4,15 @@ from word_ferry.path import get_logs_dir
 
 
 def setup_logger(name: str, type_: str, level: int = logging.INFO) -> logging.Logger:
-    log_dir = get_logs_dir()
-    logger = logging.getLogger(f"tabular_sense.{name}.{type_}")
+    log_dir = get_logs_dir() / name
+
+    if not log_dir.exists():
+        log_dir.mkdir(parents=True)
+
+    logger = logging.getLogger(f"word_ferry.{name}.{type_}")
     logger.setLevel(level)
 
-    file_handler = logging.FileHandler(log_dir / name / f"{type_}.log", encoding="utf-8")
+    file_handler = logging.FileHandler(log_dir / f"{type_}.log", encoding="utf-8")
     file_handler.setFormatter(logging.Formatter("[%(asctime)s] %(message)s"))
 
     console_handler = logging.StreamHandler()
